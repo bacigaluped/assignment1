f431b9725a85de8863e03cbec9ea04acf3ee55a8
Chase Bishop <bishop.c@husky.neu.edu>
2020-01-28 13:00:42 -0500

    updated map

--- map.h
+++ map.h
@@ -7,32 +7,32 @@ public:
   // Constructor
   Map();
 
+  // Constructor that copies existing Map
+  Map(Map* const m);
+
   // Destructor
   ~Map();
 
-  // Add a String to the map with a String key
-  void put(String* key, String* val);
-
   // Add an Object to the map with a String key
-  void put(String* key, Object* val);
+  void put(Object* const key, Object* const val);
 
   // Remove remove the value tied to the key from the map. Removes the key as well.
-  void remove(String* key);
+  Object* remove(String* const key);
 
   // Gets the value associated with the key
-  Object* get(String* key);
+  Object* get(String* const key);
 
   // Clears all the keys and values from the map
   void clear();
 
   // Is the key in the map?
-  bool containsKey(String* key);
+  bool containsKey(String* const key);
 
   // Get the number of keys in the map
   size_t size();
 
   // Is this map eual to the Object?
-  bool equals(Object* o);
+  bool equals(Object* const o);
 
   // Get the hash of the map object
   size_t hash();
